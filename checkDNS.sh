f14f7530637829c21eda7bc98ad7540cb97d95d4
hsben1012 <103327307+hsben1012@users.noreply.github.com>
2022-12-23 13:45:14 +0800

    Update checkDNS.sh

--- checkDNS.sh
+++ checkDNS.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-[ $# == "0" ] && echo 'Please input DNS Domain name U want to check.' && exit
+[ $# == "0" ] && echo 'Please input DNS domain name & key word U want to check. [./checkDNS.sh domain_name key_word]' && exit
 while true
 do
   dig @8.8.8.8 ns ${1} | grep ${2}
-    if [ $? == "0" ];then
+    if [ $? == "0" ];then #if found then break
       break
     else
-      continue
+      continue #if not found then continue
     fi
 done
 echo "End"
